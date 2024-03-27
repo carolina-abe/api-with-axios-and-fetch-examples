27cfb32f75bddbb31274a3ed8698c5b56be0845c
Carolina Abe <carolina.abe.silva@gmail.com>
2024-03-26 22:14:05 -0300

    Adds POST, PUT and DELETE

--- src/components/PostForm.jsx
+++ src/components/PostForm.jsx
@@ -1,13 +1,52 @@
-import { useState} from 'react'
+import { useEffect, useState} from 'react'
 import axios from 'axios'
 
-export const PostForm = () => {
-  const [title, setTitle] = useState("")
-  const [body, setBody] = useState("")
+export const PostForm = ({ post, onSuccess }) => {
+  const [title, setTitle] = useState(post?.title || '')
+  const [body, setBody] = useState(post?.body || '')
 
-  const handleSubmit = e => {
+  const handleSubmit = async (e) => {
     e.preventDefault()
+
+    const newPost = {title, body, userId: 1}
+
+    try {
+      if(post){
+        const response = await axios.put(`https://jsonplaceholder.typicode.com/posts/${post.id}`, newPost)
+
+        onSuccess(response.data, 'update')
+      } else {
+      const response = await axios.post('https://jsonplaceholder.typicode.com/posts', newPost)
+
+      onSuccess(response.data, 'add')
+      }
+
+      // separate this in a function
+      setTitle('')
+      setBody('')
+    } catch (error) {
+      console.log('Error: ', error)
+    }
   }
+
+  const handleDelete = async () => {
+    try {
+      await axios.delete(`https://jsonplaceholder.typicode.com/posts/${post.id}`)
+
+      onSuccess(post, 'delete')
+      setTitle('')
+      setBody('')
+    } catch (error) {
+      console.log('Delete error: ', error)
+    }
+  }
+
+  useEffect(() => {
+    if(post) {
+      setTitle(post.title)
+      setBody(post.body)
+    }
+  }, [post])
 
   return (
     <form onSubmit={handleSubmit}>
@@ -23,10 +62,13 @@ export const PostForm = () => {
         <textarea 
           value={body} 
           placeholder='Enter the content...' 
-          onChange={e => setTitle(e.target.value)}
+          onChange={e => setBody(e.target.value)}
         />
       </div>
       <button type='submit'>Edit</button>
+      {post && (
+          <button type='button' onClick={handleDelete}>Delete</button>
+      )}
     </form>
   )
 }
--- src/components/PostManager.jsx
+++ src/components/PostManager.jsx
@@ -5,6 +5,31 @@ import { PostForm } from './PostForm'
 export const PostManager = () => {
   const [posts, setPosts] = useState([])
   const [error, setError] = useState('')
+  const [selectedPost, setSelectedPost] = useState(null)
+  const [isEditing, setIsEditing] = useState(false)
+
+  const handleSuccess = (post, operation) => {
+    // refactor this with switch
+    if(operation === 'add'){
+        setPosts((curPosts) => [post, ...curPosts])
+      } else if(operation === 'update') {
+        setPosts((curPosts) => curPosts.map(p => p.id === post.id ? post : p))
+      } else if(operation === 'delete') {
+        setPosts((curPosts) => curPosts.filter(p => p.id !== post.id))
+        setSelectedPost(null)
+      }
+   setIsEditing(false)
+}
+  // refactor this 
+  const handleEdit = (post) => {
+    setSelectedPost(post)
+    setIsEditing(true)
+  }
+
+  const handleCancelEdit = () => {
+    setSelectedPost(null)
+    setIsEditing(false)
+  }
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -22,13 +47,14 @@ export const PostManager = () => {
   return (
     <div>
       <h2>Posts management</h2>
-      <PostForm />
+      <PostForm post={isEditing ? selectedPost : null} onSuccess={handleSuccess}/>
+      {isEditing && <button onClick={handleCancelEdit}>Cancel</button>}
       <h2>Posts</h2>
       {posts.map(post => (
               <div key={post.id}>
                 <h3>{post.title}</h3>
                 <span>{post.body}</span>
-                <button>Edit</button>
+                <button onClick={() => handleEdit(post)}>Edit</button>
               </div>
             )
           )}
